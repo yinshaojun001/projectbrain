2997f123a0ee82ebb3fd7d69ff314e6956496d01
yinshaojun001 <173643718@qq.com>
2026-06-14 02:33:08 +0800

    chore: release projectbrain 0.2.2

--- Formula/projectbrain.rb
+++ Formula/projectbrain.rb
@@ -3,9 +3,9 @@ class Projectbrain < Formula
 
   desc "Local-first project cognition and impact analysis layer for AI coding agents"
   homepage "https://github.com/yinshaojun001/projectbrain"
-  url "https://github.com/yinshaojun001/projectbrain/archive/de321ca616a67672a6fd6565f3af55c4240e599e.tar.gz"
-  version "0.2.1"
-  sha256 "d4124ae426221a0e67aaa7660b178c111adbd55c3e90952744c055ae88ccdb27"
+  url "https://github.com/yinshaojun001/projectbrain/archive/83712fda58d719176c899976f1c536f92d234351.tar.gz"
+  version "0.2.2"
+  sha256 "c3dea96d5d17f67a0fdc8e63914ae6b799de7b299fdca0d821d42cb5297929a7"
   license "MIT"
 
   depends_on "python@3.14"
